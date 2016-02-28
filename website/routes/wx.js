2b70a2fbf9e5fb235ab2f07c05feec07b1cd72c4
Mr-Yy <yanxinyuu@icloud.com>
2016-02-28 18:06:40 +0800

    修改

--- website/routes/wx.js
+++ website/routes/wx.js
@@ -7,12 +7,16 @@ var config = {
   encodingAESKey: 'ikd8r4AzBO2o6KZ3b9EKNcvdJHe5fWnQ5fVgwhe7eM3'
 };
 
-router.post('/', wechat(config, function(req, res, next){
+/*router.post('/', wechat(config, function(req, res, next){
 	var message = req.weixin;
 
 	if(message){
 		res.reply('success');
 	}
-}));
+}));*/
+
+router.get('/', function(req, res, next){
+	res.render('wxapp/index', { title: 'Creating' });
+})
 
 module.exports = router;
