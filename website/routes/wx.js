b3760888b03c65b7fdc87d8812a9189dbe90a34e
Mr-Yy <yanxinyuu@icloud.com>
2016-03-18 12:54:15 +0800

    接入微信

--- website/routes/wx.js
+++ website/routes/wx.js
@@ -4,19 +4,21 @@ var wechat = require('wechat');
 
 var config = {
   token: 'myweixin',
+  appid: 'wxbd50e5ea1bb8b9e4',
   encodingAESKey: 'ikd8r4AzBO2o6KZ3b9EKNcvdJHe5fWnQ5fVgwhe7eM3'
 };
 
-/*router.post('/', wechat(config, function(req, res, next){
+router.post('/', wechat(config, function(req, res, next){
 	var message = req.weixin;
 
 	if(message){
 		res.reply('success');
 	}
-}));*/
+}));
 
-router.get('/', function(req, res, next){
+/*router.get('/', function(req, res, next){
 	res.render('wxapp/index', { title: 'Creating' });
 })
+*/
 
 module.exports = router;
