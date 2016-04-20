25a58c04a30c9d402a9aaa8199fc14cfc922d690
Mr-Yy <yanxinyuu@icloud.com>
2016-04-21 00:05:32 +0800

    add mongoose

--- website/routes/wx.js
+++ website/routes/wx.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
+var mongoose = require('mongoose');
 
 var config = {
   token: 'myweixin',
@@ -16,7 +17,18 @@ router.all('/', wechat(config, function(req, res, next) {
   }
 }));
 
+var db = mongoose.connect('mongodb://wx:123456@127.0.0.1:27017/wx');
+
+var wxSchema = new mongoose.Schema({
+	wxId: String
+})
+var wxModel = db.model("WeiXin",wxSchema);
+
 router.get('/cms', function(req, res, next){
+
+	var wxEntity = new wxModel({wxId: "id23"});
+	wxEntity.save()
+
 	res.render('wxapp/index', { title: 'Creating' });
 })
 
