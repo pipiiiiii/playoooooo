8c74db611062093c0be3099a40f6acc7e233299f
Mr-Yy <yanxinyuu@icloud.com>
2016-05-21 14:54:07 +0800

    fix

--- website/routes/wx.js
+++ website/routes/wx.js
@@ -27,7 +27,7 @@ router.use('/interface/:appid', function(req, res, next){
 router.use('/interface/:appid', wechat(token).event(function(message, req, res, next){
 	var openId = message.FromUserName,
 		api = new WechatAPI(appId, appSecret);
-
+		console.log(message)
   	if (message.Event == 'subscribe'){
   		var array = [{
   			"title": "点击此处进行绑定",
